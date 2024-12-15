a5f98c8cd215c09f2d4bbaa6ca7bdd63ba3285c6
Mehul Uttam <mehulsavio@gmail.com>
2024-12-16 01:55:46 +0530

    add validation and DTO for user creation; update user endpoints and improve request handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,34 +7,41 @@ import {
   Body,
   Headers,
   Ip,
+  ParseIntPipe,
+  DefaultValuePipe,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 //http://localhost:3000/users
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/:optional?') //'/:id?' - id is optional. If id is not provided, it will return all users
+  @Get('/:id') //'/:id?' - id is optional. If id is not provided, it will return all users
   public getUsers(
     @Param() params: any,
-    @Param('id') id: any,
+    @Param('id', ParseIntPipe) id: number | undefined,
     @Query() query: any,
-    @Query('limit') limit: any,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
     console.log(params);
     console.log(query);
+
+    console.log(typeof id);
     console.log(id);
     console.log(limit);
+    console.log(page);
     return 'You sent a get request to users endpoint';
   }
 
   @Post()
   public createUser(
-    @Body() body: any,
+    @Body() createUserDto: CreateUserDto,
     @Body('email') email: any,
     @Headers() headers: any,
     @Ip() ip: any,
   ) {
-    console.log(body);
+    console.log(createUserDto);
     console.log(email);
     console.log(headers);
     console.log(ip);
