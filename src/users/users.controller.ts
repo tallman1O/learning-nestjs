75507bbe2ea30f2bf5bc63505566fe5b0dbdc6a2
Mehul Uttam <mehulsavio@gmail.com>
2024-12-15 22:07:44 +0530

    implement users controller with GET and POST endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 //http://localhost:3000/users
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  public getUsers() {
+    return 'You sent a get request to users endpoint';
+  }
+
+  @Post()
+  public createUser() {
+    return 'You sent a post request to users endpoint';
+  }
+}
