fd82f30029bbec218783445b2d63c8823fd8ca10
Mehul Uttam <mehulsavio@gmail.com>
2024-12-18 17:02:54 +0530

    enhance CreatePostDto with Swagger API properties; add optional fields for content, featuredImageURL, publishOn, tags, and metaOptions

--- src/posts/dtos/createPost.dto.ts
+++ src/posts/dtos/createPost.dto.ts
@@ -15,17 +15,27 @@ import { postStatus } from '../enum/postStatus.enum';
 import { PostType } from '../enum/postType.enum';
 import { CreatePostMetaOptionsDto } from './createPostMetaOptions.dto';
 import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreatePostDto {
+  @ApiProperty({
+    description: 'Title of the Post',
+    example: 'My First Post',
+  })
   @IsString()
   @MinLength(4)
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    enum: PostType,
+    description: "Possible Values: 'post', 'page', 'story', 'series'",
+  })
   @IsEnum(PostType)
   @IsNotEmpty()
   postType: PostType;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
@@ -34,35 +44,60 @@ export class CreatePostDto {
   })
   slug: string;
 
+  @ApiProperty()
   @IsEnum(postStatus)
   @IsNotEmpty()
   status: postStatus;
 
+  @ApiPropertyOptional()
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsJSON()
   schema?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsUrl()
   featuredImageURL?: string;
 
+  @ApiPropertyOptional()
   @IsISO8601()
   @IsOptional()
   publishOn: Date;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @MinLength(3, { each: true })
   tags?: string[];
 
+  @ApiPropertyOptional({
+    type: 'array',
+    required: false,
+    items: {
+      type: 'object',
+      properties: {
+        key: {
+          type: 'string',
+          description: 'The key of the meta option',
+          example: 'sideBarEnabled',
+        },
+        value: {
+          type: 'string',
+          description: 'The value of the meta option',
+          example: true,
+        },
+      },
+    },
+  })
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => CreatePostMetaOptionsDto)
-  metaOptions: CreatePostMetaOptionsDto[];
+  metaOptions?: CreatePostMetaOptionsDto[];
 }
